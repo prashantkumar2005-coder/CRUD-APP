0021681ecaecfabcedb337b486bbc916a4252d4a
Prashant <prashantkumar4216@gmail.com>
2026-03-10 13:32:38 +0530

    fix mongo connection

--- Frontend/services/api.js
+++ Frontend/services/api.js
@@ -1,9 +1,8 @@
- 
- import axios from "axios"
- 
- const api = axios.create({
-        baseURL: "http://localhost:3000/"
-    })
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: "http://localhost:3000/"
+})
 
 
 export default api;
